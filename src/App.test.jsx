51853fbe7179d79f656b1f89d2228d5bf672db7a
klubiniecki <klubiniecki@gmail.com>
2021-02-11 15:44:27 +1100

    update

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,7 +5,7 @@ describe('renders click me button', () => {
   it('opens overlay with text on button click', () => {
     const { getByText } = render(<App />);
     getByText('Click here');
-    fireEvent.click(getByText('Click me'));
+    fireEvent.click(getByText('Click here'));
     getByText('Nice to see you!');
   });
 });
