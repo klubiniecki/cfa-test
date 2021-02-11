06ff444ec24931e42a45141ff12c17612c70d4d0
klubiniecki <klubiniecki@gmail.com>
2021-02-11 16:00:26 +1100

    Update App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,8 +4,8 @@ import App from './App';
 describe('renders click me button', () => {
   it('opens overlay with text on button click', () => {
     const { getByText } = render(<App />);
-    getByText('Click here');
-    fireEvent.click(getByText('Click here'));
+    getByText('Click button');
+    fireEvent.click(getByText('Click button'));
     getByText('Nice to see you!');
   });
 });
