ec13bf361aef43ef3bc7ec310f95c9d3e76226fb
klubiniecki-qantas <krzysztof.lubiniecki@qantashotels.com>
2021-02-11 14:47:34 +1100

    init

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import { render, fireEvent } from '@testing-library/react';
+import App from './App';
+
+describe('renders click me button', () => {
+  it('opens overlay with text on button click', () => {
+    const { getByText } = render(<App />);
+    getByText('Click me');
+    fireEvent.click(getByText('Click me'));
+    getByText('Nice to see you!');
+  });
+});
