704b5a46451cc47158b2b8ff4465b44de60dd532
klubiniecki <klubiniecki@gmail.com>
2021-02-16 08:03:01 +1100

    add dev flag

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,10 @@ import Button from '../Button/Button';
 import Alert from '../Alert/Alert';
 
 function App() {
+  if (!process.env.REACT_APP_DEV) {
+    return <div>Work in progress</div>;
+  }
+
   return (
     <div className="App">
       <h1>My app title</h1>
